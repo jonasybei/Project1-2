a3cb551deef27914bdfe771335ec4ac844bd1446
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2018-03-19 15:23:22 +0100

    Made the Terrain class that holds the functions to compute the terrain

---
+++ Terrain.java
@@ -0,0 +1,6 @@
+//This class computes the terrain with the use of functions. Those can be found here:
+//https://docs.google.com/document/d/1ZUv9NAnBHs384mxa-9tkk39cExyGSKAK1exx1yJGvn0/edit?usp=sharing
+
+public class Terrain {
+	
+}
