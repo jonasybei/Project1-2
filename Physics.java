3bfce996481b6212659d267439b275d6bff56830
gabrifresia <37535402+gabrifresia@users.noreply.github.com>
2018-03-22 11:50:46 +0100

    Add files via upload

---
+++ Physics.java
@@ -0,0 +1,48 @@
+import java.lang.Math;
+public class Physics {
+  final double g = 9.81;
+  final double h = 0.0125;
+  double mu;
+  double x = 0;
+  double y = 0;
+  //double z = (function);
+  double safex = 0;
+  double safey = 0;
+  double lastz = 0;
+  double vtot,vx,vy,vz;
+  //DISPLAYBALL
+  boolean target = true;
+  do {
+    //vtot=input;
+    //alfa=input;
+    vx=vtot*cos(alfa);
+    vy=vtot*sin(alfa);
+    vz=0;
+    do {
+      x=x+h*vx;
+      y=y+h*vy;
+      //z=function
+      //DISPLAYBALL
+      mu = checkFriction(x,y);
+      vx=vx-mu*g*h;
+      vy=vy-mu*g*h;
+      vz=vz-g*h*(z-lastz);
+      vtot = sqrt(pow(vx,2)+pow(vy,2)+pow(vz,2));
+      if (z<0) {
+        vtot=0
+      }
+    } while (vtot>0)
+    if (z<0) {
+      x=safex;
+      y=safey;
+      //z=function;
+      //DISPLAYBALL
+    }
+    else {
+      safex=x;
+      safey=y;
+    }
+    //target = checkTarget(x,y);
+  } while (target)
+
+}
