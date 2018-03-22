9db14f751cdf9afcebce36ac51a099db373b1d61
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2018-03-22 17:52:16 +0100

    Fixed the functions for terrain

--- Terrain.java
+++ Terrain.java
@@ -9,34 +9,28 @@ import java.lang.Math.*;
 //All terrains are meant to have a range of -10 to 10 for x, y, and z!
 
 public class Terrain {
+
 	public static Double compute(int terrain, double x, double y) {
 		if (terrain == 1) {
-			//(e^x-e^y)/2000 - 5*e^(-x^2-y^2)
-			return (((Math.exp(x)-Math.exp(y))/2000)-(5*Math.exp(-Math.pow(x, 2)-Math.pow(y, 2))));
+			//(e^x-e^y)/2000-5*e^(-(x+5)^2-(y+2)^2)
+			return ((Math.exp(x) - Math.exp(y))/2000 - 5*Math.exp(-Math.pow((x+5), 2) - Math.pow((y+2), 2)));
 		} else if(terrain == 2) {
 			//(x^2+y^3)/100 + 6*e^(-x^2-y^2)
 			return ((Math.pow(x, 2) + Math.pow(y, 3))/100 + 6*Math.exp(-Math.pow(x, 2)-Math.pow(y, 2)));
 		} else if(terrain == 3) {
 			//abs(-x^3-y^3+5(-e^(-(x-2)^2-(y+2)^2)*10^2.5))-e^(-x^2-y^2)*10^2.8
-<<<<<<< HEAD
-			//return null;
-			return 1;
-=======
-			return null;
->>>>>>> 3bfce996481b6212659d267439b275d6bff56830
+			return (Math.abs(-Math.pow(x, 3)-Math.pow(y, 3) + 5*(-Math.exp(-(Math.pow((x-2), 2)-Math.pow((y+2), 2)))*Math.pow(10, 2.5))) -Math.exp(-Math.pow(x, 2)-Math.pow(y, 2))*Math.pow(10, 2.8));
 		} else if (terrain == 4) {
 			//abs(x^2+y^3)/100+abs(6*e^(-x^2-y^2))
 			return (Math.abs(Math.pow(x, 2) + Math.pow(y, 3))/100 + Math.abs(6*Math.exp(-Math.pow(x, 2)-Math.pow(y, 2))));
 		} else if (terrain == 5) {
-			//-sin(y/5)+1.5*e^(-x^2-y^2)+0.5
-			return (-Math.sin(y/5) + 1.5*Math.exp(-Math.pow(x, 2)-Math.pow(y, 2)) + 0.5);
+			//-sin(y/5)+1.5*e^(-(x)^2-(y)^2)+1.5*e^(-(x+4)^2-(y+4)^2)+1.5*e^(-(x-4)^2-(y-4)^2)+0.5
+			return (-Math.sin(y/5) + 1.5*Math.exp(-Math.pow(x, 2)-Math.pow(y, 2)) + 1.5*Math.exp(-Math.pow((x+4), 2)-Math.pow((y+4), 2)) + 1.5*Math.exp(-Math.pow((x-4), 2)-Math.pow((y-4), 2)) + 0.5);
 		} else if( terrain == 6) {
 			//abs(x/y)/2
 			return (Math.abs(x/y)/2);
 		}
-<<<<<<< HEAD
-		return 1;
-		//return null;
+		return null;
 	}
 
 
