a03d365562e9ec97cef6ae5217215a5b248b9b84
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2018-04-24 14:38:15 +0200

    Added the first version of the bot

---
+++ GolfBot.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class GolfBot {
+
+  public static int[] play(double startX, double startY, double goalX, double goalY, int terrain){
+    //if we can reach the goal in one shot, go for it!
+    if(canReachGoal(startX, startY, goalX, goalY, terrain)) {
+      //compute power
+      //shoot
+    } else {
+
+    }
+    return null;
+  }
+
+  public static boolean canReachGoal(double pointX, double pointY, double goalX, double goalY, int terrain){
+    int steps = 100; //This is wrong! MUST BE CHANGED!
+
+    for(int i = 1; i < steps; i++) {
+      double t = i/steps;
+      if(Terrain.compute(terrain, (pointX + t * (goalX - pointX)), (pointY + t * (goalY - pointY))) < 0) {
+
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(canReachGoal(7.5, 0, 7.5, -2.5, 1));
+  }
+
+}
