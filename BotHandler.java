b8fd631f2cd02d83d25600d10c5e25b62e621ed7
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2018-05-16 16:00:41 +0200

    Fixed some bugs

--- BotHandler.java
+++ BotHandler.java
@@ -1,9 +1,10 @@
 import com.badlogic.gdx.math.Vector2;
 
 public class BotHandler {
-  public Shot[] DCAI(Vector2 aStart, Vector2 aGoal){
+  public Shot[] DCAI(int terrain, Vector2 aStart, Vector2 aGoal){
+    DCBot bot = new DCBot(terrain);
     Point start = new Point(aStart.x, aStart.y);
     Point goal = new Point(aGoal.x, aGoal.y);
-    return DCBot.compute(start, goal);
+    return bot.compute(start, goal);
   }
 }
